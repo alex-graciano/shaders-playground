315a4c2c6b4ebe8e10ff9e064b1f25b14ba8f8bc
Alex Graciano <agracianosegura@gmail.com>
2024-05-02 11:51:18 +0200

    feat: add tweakpane

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Shader playground
 
-GLSL shader playground. Repository aimed to test simple GLSL ideas (mainly for fragment shaders). This contains the most straightforward skeleton to execute GLSL using the WebGL library [Three.js](https://threejs.org/). The web app is being bundled in a minimal [Express](https://expressjs.com/) server.
+GLSL shader playground. Repository aimed to test simple GLSL ideas (mainly for fragment shaders). This contains the most straightforward skeleton to execute GLSL using the WebGL library [Three.js](https://threejs.org/). The web app is being bundled in a minimal [Webpack](https://webpack.js.org/) server.
+
+It uses the simple [Tweakpane](https://tweakpane.github.io/docs/) lib to link parameters to be tested in the sketch in a straightforward way.
 
 ### Execution
 ```
--- public/scene.js
+++ public/scene.js
@@ -1,53 +1,69 @@
-var container;
-var camera, scene, renderer, clock;
-var uniforms;
+import { Pane } from 'tweakpane'
 
-init().then(() => animate());
+let container;
+let camera, scene, renderer, clock;
+let uniforms;
 
-async function init() {
-    container = document.body;
+const params = {
+    cols: 10,
+    rows: 10,
+    scaleMin: 1,
+    scaleMax : 30,
+    frequency: 0.001,
+    amplitude: 0.2,
+    animate: true,
+    frame: 0,
+    lineCap: 'butt'
+  }
+
+  
+  async function init() {
+      container = document.body;
 
     camera = new THREE.Camera();
     camera.position.z = 1;
 
     scene = new THREE.Scene();
     clock = new THREE.Clock();
-
-    var geometry = new THREE.PlaneBufferGeometry( 2, 2 );
-
+    
+    const geometry = new THREE.PlaneBufferGeometry( 2, 2 );
+    
+    const vShader = './shaders/vertex.glsl';
+    const fShader = './shaders/fragment.glsl';
+    
     uniforms = {
         u_time: { type: "f", value: 1.0 },
         u_resolution: { type: "v2", value: new THREE.Vector2() },
         u_mouse: { type: "v2", value: new THREE.Vector2() }
     };
+    
+    const vertexShader = await fetch(vShader);
+    const vertexShaderText = await vertexShader.text();
 
-    var vertexShader = await fetch('./shaders/vertex.glsl');
-    var vertexShaderText = await vertexShader.text();
-
-    var fragmentShader = await fetch('./shaders/fragment.glsl');
-    var fragmentShaderText = await fragmentShader.text();
-
-
-    var material = new THREE.ShaderMaterial( {
+    const fragmentShader = await fetch(fShader);
+    const fragmentShaderText = await fragmentShader.text();
+    
+    
+    const material = new THREE.ShaderMaterial( {
         uniforms: uniforms,
         vertexShader: vertexShaderText,
         fragmentShader: fragmentShaderText
     } );
 
-    var mesh = new THREE.Mesh( geometry, material );
+    const mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
 
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio( window.devicePixelRatio );
-
+    
     container.appendChild( renderer.domElement );
-
+    
     onWindowResize();
     window.addEventListener( 'resize', onWindowResize, false );
-
+    
     document.onmousemove = function(e){
-      uniforms.u_mouse.value.x = e.pageX
-      uniforms.u_mouse.value.y = e.pageY
+        uniforms.u_mouse.value.x = e.pageX
+        uniforms.u_mouse.value.y = e.pageY
     }
 }
 
@@ -66,3 +82,25 @@ function render() {
     uniforms.u_time.value += clock.getDelta();
     renderer.render( scene, camera );
 }
+
+function createGUI() {
+    const pane = new Pane();
+    let folder;
+
+    folder = pane.addFolder({ title: 'Grid' });
+    folder.addBinding(params, 'lineCap', {options: {butt: 'butt', round: 'round', square: 'square'}});
+    folder.addBinding(params, 'cols', {min: 2, max: 50, step: 1});
+    folder.addBinding(params, 'rows', {min: 2, max: 50, step: 1});
+    folder.addBinding(params, 'scaleMin', {min: 1, max: 100, step: 1});
+    folder.addBinding(params, 'scaleMax', {min: 1, max: 100, step: 1});
+    
+    folder = pane.addFolder({ title: 'Noise' });
+    folder.addBinding(params, 'frequency', {min: -0.01, max: 0.01});
+    folder.addBinding(params, 'amplitude', {min: 0, max: 1});
+    folder.addBinding(params, 'animate');
+    folder.addBinding(params, 'frame', {min: 0, max: 999});
+    
+}
+
+createGUI();
+init().then(() => animate());
