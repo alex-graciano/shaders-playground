5d1ef785a8d50dff03d21d6c9ff3081b43625137
Alex Graciano <agracianosegura@gmail.com>
2024-05-02 11:50:56 +0200

    chore: change express to webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './public/scene.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  devServer: {
+    static: {
+        directory: path.join(__dirname, 'public'),
+    },    compress: true,
+    port: 8080,
+    open: true,
+    historyApiFallback: {
+        index: 'sketch.html'
+    }
+  },
+  devtool: "source-map"
+};
